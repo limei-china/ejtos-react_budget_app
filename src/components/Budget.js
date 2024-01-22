2aa393b5417a0dda156d106a5015a8b1c89629dd
limei-china <shen18071510@gmail.com>
2024-01-22 01:05:27 -0500

    Add budget limit.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,9 +3,19 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, currency } = useContext(AppContext);
+    const { budget, currency, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
+        if(event.target.value > 20000) {
+            alert("The value cannot exceed the upper limit "+currency+" 20000");
+            return;
+        } else if(event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
         setNewBudget(event.target.value);
     }
     return (
